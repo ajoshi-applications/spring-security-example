44958de57a9fb328d7b48df23f7ce28d43b4a155
arturjoshi <arturjoshi.mailbox@gmail.com>
2016-12-28 22:44:48 +0200

    Revert "custom authentication"
    This reverts commit 4b0c01c7d12c0aa434f9ca9971e26ab6eb11e641.

--- src/main/java/edu/arturjoshi/config/WebSecurityConfig.java
+++ src/main/java/edu/arturjoshi/config/WebSecurityConfig.java
@@ -1,13 +1,10 @@
 package edu.arturjoshi.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by arturjoshi on 25-Dec-16.
@@ -15,31 +12,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/users", "/securitycontext", "/register")
-                .permitAll()
                 .anyRequest().authenticated()
-                .and().httpBasic()
-                .and().csrf().disable();
+                .and().httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(new BCryptPasswordEncoder());
-//        auth.inMemoryAuthentication()
-//                .withUser("admin")
-//                .password("admin")
-//                .roles("ADMIN", "USER")
-//                .and()
-//                .withUser("user")
-//                .password("user")
-//                .roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN", "USER")
+                .and()
+                .withUser("user")
+                .password("user")
+                .roles("USER");
     }
 }
--- src/main/java/edu/arturjoshi/domain/User.java
+++ src/main/java/edu/arturjoshi/domain/User.java
@@ -1,12 +1,8 @@
 package edu.arturjoshi.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import edu.arturjoshi.authentication.UserRegistrationDto;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,27 +13,12 @@ import javax.persistence.Transient;
  * Created by arturjoshi on 25-Dec-16.
  */
 @Entity
-@Data
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class User {
     @Id @GeneratedValue
     private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    @JsonIgnore
-    private String password;
-
-    public User(User user) {
-        this.id = user.id;
-        this.firstName = user.firstName;
-        this.lastName = user.lastName;
-        this.email = user.email;
-        this.password = user.password;
-    }
-
-    public User(UserRegistrationDto userDto) {
-        this.email = userDto.getEmail();
-        this.password = userDto.getPassword();
-    }
+    private @NonNull String username;
+    @Transient
+    private @NonNull String password;
 }
