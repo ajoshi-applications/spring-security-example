71d42c91e2ad84da3c9eb2cb523948c9f8ed5726
arturjoshi <arturjoshi.mailbox@gmail.com>
2016-12-28 22:52:52 +0200

    ok

---
+++ src/main/java/edu/arturjoshi/authentication/UserRegistrationDto.java
@@ -0,0 +1,14 @@
+package edu.arturjoshi.authentication;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by arturjoshi on 26-Dec-16.
+ */
+@NoArgsConstructor
+@Data
+public class UserRegistrationDto {
+    private String email;
+    private String password;
+}
--- src/main/java/edu/arturjoshi/config/WebSecurityConfig.java
+++ src/main/java/edu/arturjoshi/config/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package edu.arturjoshi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by arturjoshi on 25-Dec-16.
@@ -12,22 +15,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/users", "/securitycontext", "/register")
+                .permitAll()
                 .anyRequest().authenticated()
-                .and().httpBasic();
+                .and().httpBasic()
+                .and().csrf().disable();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("admin")
-                .roles("ADMIN", "USER")
-                .and()
-                .withUser("user")
-                .password("user")
-                .roles("USER");
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+//        auth.inMemoryAuthentication()
+//                .withUser("admin")
+//                .password("admin")
+//                .roles("ADMIN", "USER")
+//                .and()
+//                .withUser("user")
+//                .password("user")
+//                .roles("USER");
     }
 }
